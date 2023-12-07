5b00ec3183482255e6516f26e3969c8ddb06d584
emanuel0004 <emanuel.zetka@accusys.com.ar>
2023-12-07 07:21:56 -0300

    se agrego firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -39,6 +40,9 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation ("com.github.bumptech.glide:glide:4.13.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-analytics")
     implementation( "com.fasterxml.jackson.core:jackson-databind:2.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/davinci/pokedex/Login.java
+++ app/src/main/java/com/davinci/pokedex/Login.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Initialize Firebase Auth
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         Button button = findViewById(R.id.omitir);
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +32,16 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(Login.this, RegionActivity.class);
+            startActivity(intent);
+        }
     }
 }
